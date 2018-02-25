ce99baf6663c06f9d24990cc1723385607480b83
Logan Olzenak <logan.olzenak@gmail.com>
2018-02-25 15:53:06 -0600

    finished

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.json());
 
 
-
+const myLife = require('./router/myLifeRouter');
 
 
 
 app.use(express.static('server/public'));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log(`Listening on port ${port}.`);
 })
